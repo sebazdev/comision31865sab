9a6c10252809b37e9b534165b6c2be83649333ea
Sebastian Zuviria <szuviria@gmail.com>
2022-05-14 15:43:18 -0300

    Instalacion y configuracion del entorno

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Comision 31865</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hola Coders
         </p>
         <a
           className="App-link"
